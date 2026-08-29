6e5c97a773b4d02d4e99967eb6edfdf5c5a3dbf1
Davi Silva <davisilvaphoto@gmail.com>
2026-08-29 10:19:12 -0300

    feat: create session guard

---
+++ api-gateway/src/auth/guard/session.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth/auth.service';
+
+@Injectable()
+export class SessionGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const sessionToken = request.headers['x-session-token'];
+
+    if (!sessionToken)
+      throw new UnauthorizedException('session token required');
+
+    try {
+      const session = await this.authService.validateSessionToken(sessionToken);
+
+      if (!session.valid || !session.user)
+        throw new UnauthorizedException('invalid token token');
+
+      request.user = session.user;
+      return true;
+    } catch {
+      throw new UnauthorizedException('session token required');
+    }
+  }
+}
